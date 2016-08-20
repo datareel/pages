3c44b107b926d4714b05faf16fe64412a6259f7a
dgaerhome <dgaerhome@gmail.com>
2016-08-20 16:33:20 -0500

    updated datareel website

---
+++ js/all_pages.js
@@ -0,0 +1,60 @@
+// ------------------------------- //
+// -------- Start of File -------- //
+// ------------------------------- //
+// ----------------------------------------------------------- // 
+// JavaScript Page
+// Designed For: Internet Explorer, Netscape, Firefox, Chrome
+// Published By: DataReel Software Development
+// File Creation Date: 11/04/2001
+// Date Last Modified: 10/02/2008
+//
+// Package version: 1.01
+//
+// Copyright (c) 2001-2008 DataReel Software Development
+// ----------------------------------------------------------- // 
+// -------------- Script Description and Details ------------- // 
+// ----------------------------------------------------------- // 
+/*
+START OF LICENSING AND DISCLAIMER AGREEMENT
+
+This JavaScript page and its derivatives are copyright (c) 2001-2008,
+by DataReel Software Development. This JavaScript page and any
+derivative Web objects are published on the Internet for public
+viewing only. Reproduction of any JavaScript page and/or Web objects
+published on this Website is prohibited unless authorized in writing
+by DataReel Software Development or an authorized reseller. Reproduction
+of any third party code used to produce pages on this Website is
+subject to the third party licensing and distribution agreement.
+
+THIS SOFTWARE IS PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND. THE
+ENTIRE RISK OF THE QUALITY AND PERFORMANCE OF THIS SOFTWARE IS WITH
+YOU. SHOULD ANY ELEMENT OF THIS SOFTWARE PROVE DEFECTIVE, YOU WILL
+ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+***********************************************************
+*************** THIRD PARTY ACKNOWLEDGMENT ****************
+***********************************************************
+
+NONE
+
+END OF LICENSING AND DISCLAIMER AGREEMENT
+
+JavaScript include file
+*/
+// ----------------------------------------------------------- // 
+// SCRIPT CONTENT ENDS BELOW
+
+// Document modification dates
+var modDate = new Date(document.lastModified);
+var modMonth = modDate.getMonth()+1;
+var modYear = modDate.getYear();
+var modDay = modDate.getDate();
+if(modDay<10) modDay = "0" + modDay;
+if(modMonth<10) modMonth= "0" + modMonth; 
+if(modYear<1000) modYear+=1900;
+
+// SCRIPT CONTENT ENDS ABOVE
+// ----------------------------------------------------------- //
+// ------------------------------- //
+// --------- End of File --------- //
+// ------------------------------- //
